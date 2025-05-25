7c4199c986bdd410273084a97309f030dd582bc7
Aryeh Silver <aryehsilver@hotmail.com>
2025-05-25 14:27:09 +0100

    Finish off dark mode settings

--- AlHatorah/App.xaml.cs
+++ AlHatorah/App.xaml.cs
@@ -3,6 +3,7 @@
 public partial class App : Application {
   public static string StartUrl = "https://alhatorah.org";
   public static bool SetUrl = false;
+  public static bool DarkMode = false;
 
   public App() =>
     InitializeComponent();
--- AlHatorah/MainPage.xaml.cs
+++ AlHatorah/MainPage.xaml.cs
@@ -60,18 +60,39 @@ public partial class MainPage : ContentPage {
     Toast.MakeText(MainActivity.Instance, $"{result} saved", ToastLength.Long).Show();
   }
 
-  private async void OnNavigated(object sender, WebNavigatedEventArgs e) =>
-    // TODO: Make this a floating button that saves to preferences and load accordingly
-    await webView.EvaluateJavaScriptAsync(@$"
-      document.body.style.backgroundColor = ""#3c3c3c"";
-      var elements = document.getElementsByClassName(""verse"");
-      for (var i = 0; i < elements.length; i++) {{
-        elements[i].style.background = ""#242424"";
-        elements[i].style.color = ""#cdcdcd"";
-      }}
-      document.getElementsByClassName(""topbar-container"")[0].style.background = ""linear-gradient(#531b1b, #350101)"";
-      const style = document.createElement(""style"");
-      style.appendChild(document.createTextNode("".content:before {{ color: #b7b7b7 !important; }} .sidebar.active {{ background-color: #484848; }} .sidebar a:not(.close) {{ background: #999999; }} .sidebar a:not(.close).active {{ background: #282828; }}""));
-      document.getElementsByTagName(""body"")[0].appendChild(style);
-    ");
+  private async void OnNavigated(object sender, WebNavigatedEventArgs e) {
+    if (Preferences.Get($"AH{nameof(App.DarkMode)}", false)) {
+      App.DarkMode = true;
+      await ToggleDarkMode(App.DarkMode);
+    }
+  }
+
+  private async void DarkMode_Clicked(object sender, EventArgs e) {
+    bool darkMode = !App.DarkMode;
+    await ToggleDarkMode(darkMode);
+  }
+
+  private async Task ToggleDarkMode(bool darkMode) {
+    Preferences.Default.Set($"AH{nameof(App.DarkMode)}", darkMode);
+    if (darkMode) {
+      await webView.EvaluateJavaScriptAsync(@$"
+        document.body.style.backgroundColor = ""#3c3c3c"";
+        document.getElementsByClassName(""topbar-container"")[0].style.background = ""linear-gradient(#531b1b, #350101)"";
+        const style = document.createElement(""style"");
+        style.appendChild(document.createTextNode(""body:not(.copying-to-clipboard) .verse, .options-place {{ background: #242424; color: #cdcdcd; }} .options-place-list li {{ background: #3c3c3c; border-color: #757575 !important; }} .mg-dlg .close {{ color: #ffffff; }} .options-heading {{ color: #b90000; }} .options-place-list li.options-place-selected, .home-section {{ background: #282828; color: #d77070; }} .options-place-chapter:before {{ background: #282828; }} .content:before {{ color: #b7b7b7 !important; }} .sidebar.active {{ background-color: #484848; }} .sidebar a:not(.close) {{ background: #999999; }} .sidebar a:not(.close).active {{ background: #282828; }}"")); document.getElementsByTagName(""body"")[0].appendChild(style);");
+      SetButtonsColours(true);
+    } else {
+      SetButtonsColours(false);
+      webView.Reload();
+    }
+  }
+
+  private void SetButtonsColours(bool dark) {
+    darkButton.Text = dark ? "☀️" : "🌙";
+    darkButton.Background = Color.FromArgb(dark ? "#282828" : "#fff");
+    darkButton.BorderColor = Color.FromArgb(dark ? "#757575" : "#b5b5b5");
+    saveButton.Background = Color.FromArgb(dark ? "#282828" : "#fff");
+    saveButton.BorderColor = Color.FromArgb(dark ? "#757575" : "#b5b5b5");
+    saveButton.TextColor = Color.FromArgb(dark ? "#fff" : "#000");
+  }
 }
